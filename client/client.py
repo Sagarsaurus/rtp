390ab7007c882d4897370701189cc32f0a374d0e
Sagarsaurus <sagrsccr@gmail.com>
2014-11-21 15:23:34 -0500

    Code is currently broken, working on 3 way handshake, currently deadlocks

--- client/client.py
+++ client/client.py
@@ -1,4 +1,5 @@
 import socket
+from struct import *
 
 # # creating the socket for TCP
 # client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,7 +14,8 @@ import socket
 
 
 class client:
-
+	expected_sequence_number=100
+	expected_ack_number=0
 	connected = False
 	client_socket = None
 	port=0
@@ -29,6 +31,32 @@ class client:
 		self.dest_port=dest_port
 		self.dest_ip=dest_ip
 	#def connect(dest_port, dest_ip):
+
+	def connect(self, port, dest_port, dest_ip):
+		synacked=False
+		self.client_socket.settimeout(1)
+		while not synacked:
+			try: 	
+				p=packet(port, dest_port, 1, 0, 1, 0, 0, 0, 0, 1234, 50, 'syn packet')
+				packed = pack('iiiiiiiiiiis', port, dest_port, 1, 0, 1, 0, 0, 0, 0, 1234, 50, 'syn packet')
+				expected_ack_number=2
+				self.client_socket.sendto(packed, ('', 4001))
+				response, address = self.client_socket.recvfrom(512)
+				#perform checksum for ack
+				#check to see if syn and ack AND values for sequence number and ack fields match with what we expect
+				ack_packet=packet(response[0], response[1], response[2], response[3], response[4], response[5], response[6], response[7], response[8], response[9], response[10], response[11])
+				expected_sequence_number=ack_packet.seq_num
+				if ack_packet.syn==1 and ack_packet.ack==1 and ack_packet.ack_num==(p.seq_num+1):
+					connected=True
+					synacked=True
+					print ack_packet			
+			except socket.timeout:
+				continue
+		send_message()
+
+	def send_message(self):
+		print 'begin sending message after successful connection'
+
 
 # Packet Header
 class packet:
@@ -48,13 +76,12 @@ class packet:
 		self.fcw = fcw
 		self.data = data
 
-p = packet(4000, 4001, 1, 0, 0, 0, 0, 0, 0, 1234, 0, "message")
+
+
+
+
 client_object = client(4000, 7000, '143.215.129.100')
-client_object.client_socket.sendto(p.data, (client_object.dest_ip, client_object.dest_port))
-
-
-
-
+client_object.connect(4000, 4001, '')
 
 
 
--- server/server.py
+++ server/server.py
@@ -1,4 +1,5 @@
 import socket
+from struct import *
 
 # Packet Header
 class packet:
@@ -20,7 +21,9 @@ class packet:
 
 
 class server:
-
+	expected_seq_number=0
+	expected_ack_number=0
+	seq_num=100
 	connected = False
 	server_socket = None
 
@@ -32,13 +35,48 @@ class server:
 
 		self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		try:
-			self.server_socket.bind(('', 4001))
+			self.server_socket.bind(('', port))
 		except:
 			print "failed bind"
 
+	def connect(self):
+		self.server_socket.settimeout(5)
+		p, address = self.server_socket.recvfrom(512)
+		response = unpack('iiiiiiiiiiis', p)
+		client_packet=packet(response[0], response[1], response[2], response[3], response[4], response[5], response[6], response[7], response[8], response[9], response[10], response[11])
+		firstDataReceived=None
+		#check checksum
+		#make sure all values match up: is syn packet, then assign acknum field with seq+1
+		while not self.connected:
+			try:
+				if client_packet.syn==1:
+					print client_packet
+					expected_seq_number=client_packet.seq_num+1
+					expected_ack_number=self.seq_num+1
+					response = pack('iiiiiiiiiiis', 4001, 4000, self.seq_num, client_packet.seq_num+1, 1, 1, 0, 0, 0, 1432, 50, 'synack packet')
+					self.server_socket.sendto(response, ('', 4000))
+					checkPacket, address = self.server_socket.recvfrom(512)
+					firstDataReceived=checkPacket
+					#check again for corruption
+					response = unpack('iiiiiiiiiiis', checkPacket)
+					client_packet=packet(response[0], response[1], response[2], response[3], response[4], response[5], response[6], response[7], response[8], response[9], response[10], response[11])
+
+				elif client_packet.ack==1:
+					connected=True
+
+
+			except socket.timeout:
+				continue
+		beginTransmission(firstDataReceived)
+
+	def beginTransmission(self, firstData):
+		print "yay we're connected"
+
+
+
+
 
 	def receive(self):
-		print 'got here'
 
 		received = False
 		# Just initialized
@@ -48,6 +86,9 @@ class server:
 
 			# Check if the data is a packet or not
 			p, address = self.server_socket.recvfrom(512)
+			response = unpack('iiiiiiiiiiis', p)
+			syn_packet=packet(response[0], response[1], response[2], response[3], response[4], response[5], response[6], response[7], response[8], response[9], response[10], response[11])
+
 
 			# Perform checksum stuff
 			# init random seq_num
@@ -60,8 +101,6 @@ class server:
 			#	ack_packet = packet(port, dest_port, seq_num, data.seq_num + 1, 1, 1, 0, 0, 0, checksum, fcw, data)
 			#	server_socket.sendto(ack_packet, address)
 
-			print p
-
 
 s = server(4001, 7000, "143.215.129.100");
-s.receive()
+s.connect()
