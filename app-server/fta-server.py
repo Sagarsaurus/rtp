6051e2950b41a28b05dd5fb0337d6c31d00ddde9
Samarth <samarth.agarwal1993@gmail.com>
2014-11-27 20:06:40 -0500

    Commit to pull new changes

--- app-server/fta-server.py
+++ app-server/fta-server.py
@@ -14,7 +14,7 @@ class appserver:
 		self.terminated = False
 		self.connected = False
 
-		while (not self.terminated):
+		while (not self.connected):
 
 			inputs = raw_input('>>> ')
 			inputs = inputs.split()
@@ -33,13 +33,22 @@ class appserver:
 				self.server_socket = server(self.port, self.dest_port, self.dest_ip)
 				self.startServer()
 
-
-			elif (inputs[0] == 'terminate'):
-				terminated = True
+		self.running()
 
 	def startServer(self):
 		connected = self.server_socket.connect() #Same as listen
-		return connected
+		self.connected = connected
+
+	def running(self):
+
+		while not self.terminated:
+
+			inputs = raw_input('>>> (Type y to continue): ')
+			inputs = inputs.split()
+
+			if (inputs[0] == 'y'):
+				message = self.server_socket.receive()
+				print message
 
 server = appserver()
 
